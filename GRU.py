219e29eb304fb3b61774d80ec8f6c2877ed0e90d
khuangaf <khuangaf@connect.ust.hk>
2017-12-30 21:04:34 +0800

    test GRU -> better than LSTM

--- GRU.py
+++ GRU.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as numpy
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Activation, Flatten,Reshape
-from keras.layers import Conv1D, MaxPooling1D
+from keras.layers import Conv1D, MaxPooling1D, LeakyReLU
 from keras.utils import np_utils
 from keras.layers import GRU
 from keras.callbacks import CSVLogger, ModelCheckpoint
@@ -21,18 +21,18 @@ config = tf.ConfigProto()
 config.gpu_options.allow_growth = True
 set_session(tf.Session(config=config))
 
-with h5py.File(''.join(['bitcoin2012_2017_256_16.h5']), 'r') as hf:
+with h5py.File(''.join(['bitcoin2015to2017_close.h5']), 'r') as hf:
     datas = hf['inputs'].value
     labels = hf['outputs'].value
 
 
-output_file_name='bitcoin2012_2017_256_16_GRU_RS'
+output_file_name='bitcoin2015to2017_close_GRU_1_tanh_leaky_'
 
 step_size = datas.shape[1]
-units= 1
+units= 50
 batch_size = 8
 nb_features = datas.shape[2]
-epochs = 1000
+epochs = 100
 output_size=16
 #split training validation
 training_size = int(0.8* datas.shape[0])
@@ -43,16 +43,14 @@ validation_labels = labels[training_size:,:,0]
 
 #build model
 model = Sequential()
-model.add(GRU(units=units,activation='tanh', input_shape=(step_size,4),return_sequences=True))
+model.add(GRU(units=units,activation='tanh', input_shape=(step_size,nb_features),return_sequences=False))
 model.add(Dropout(0.2))
-model.add(Reshape((-1,)))
 model.add(Dense(output_size))
-
-model.add(Activation('sigmoid'))
+model.add(LeakyReLU())
 model.compile(loss='mse', optimizer='adam')
-model.fit(training_datas, training_labels, batch_size=batch_size,validation_data=(validation_datas,validation_labels), epochs = epochs, callbacks=[CSVLogger(output_file_name+'.csv', append=True),ModelCheckpoint('weights/'+output_file_name+'weights-improvement-{epoch:02d}-{val_loss:.5f}.hdf5', monitor='val_loss', verbose=1,mode='min')])
+model.fit(training_datas, training_labels, batch_size=batch_size,validation_data=(validation_datas,validation_labels), epochs = epochs, callbacks=[CSVLogger(output_file_name+'.csv', append=True),ModelCheckpoint('weights/'+output_file_name+'-{epoch:02d}-{val_loss:.5f}.hdf5', monitor='val_loss', verbose=1,mode='min')])
 
 # model.fit(datas,labels)
-model.save(output_file_name+'.h5')
+#model.save(output_file_name+'.h5')
 
 
--- LSTM.py
+++ LSTM.py
@@ -4,7 +4,7 @@ from keras.models import Sequential
 from keras.layers import Dense, Dropout, Activation, Flatten,Reshape
 from keras.layers import Conv1D, MaxPooling1D
 from keras.utils import np_utils
-from keras.layers import LSTM
+from keras.layers import LSTM, LeakyReLU
 from keras.callbacks import CSVLogger, ModelCheckpoint
 import h5py
 import os
@@ -21,7 +21,7 @@ config = tf.ConfigProto()
 config.gpu_options.allow_growth = True
 set_session(tf.Session(config=config))
 
-with h5py.File(''.join(['bitcoin2012_2017_256_16.h5']), 'r') as hf:
+with h5py.File(''.join(['bitcoin2015to2017_close.h5']), 'r') as hf:
     datas = hf['inputs'].value
     labels = hf['outputs'].value
 
@@ -35,7 +35,7 @@ batch_size = 8
 nb_features = datas.shape[2]
 epochs = 100
 output_size=16
-output_file_name='bitcoin2012_2017_256_16_LSTM_50_30'
+output_file_name='bitcoin2015to2017_close_LSTM_1_tanh_leaky_'
 #split training validation
 training_size = int(0.8* datas.shape[0])
 training_datas = datas[:training_size,:]
@@ -43,16 +43,15 @@ training_labels = labels[:training_size,:,0]
 validation_datas = datas[training_size:,:]
 validation_labels = labels[training_size:,:,0]
 
+
 #build model
 model = Sequential()
-model.add(LSTM(units=units,activation='tanh', input_shape=(step_size,4),return_sequences=True))
-model.add(LSTM(units=second_units,activation='relu',return_sequences=False))
+model.add(LSTM(units=units,activation='tanh', input_shape=(step_size,nb_features),return_sequences=False))
 model.add(Dropout(0.2))
-model.add(Reshape((-1,)))
 model.add(Dense(output_size))
-model.add(Activation('sigmoid'))
+model.add(LeakyReLU())
 model.compile(loss='mse', optimizer='adam')
-model.fit(training_datas, training_labels, batch_size=batch_size,validation_data=(validation_datas,validation_labels), epochs = epochs, callbacks=[CSVLogger(output_file_name+'.csv', append=True),ModelCheckpoint('weights/'+output_file_name+'weights-improvement-{epoch:02d}-{val_loss:.5f}.hdf5', monitor='val_loss', verbose=1,mode='min')])
+model.fit(training_datas, training_labels, batch_size=batch_size,validation_data=(validation_datas,validation_labels), epochs = epochs, callbacks=[CSVLogger(output_file_name+'.csv', append=True),ModelCheckpoint('weights/'+output_file_name+'-{epoch:02d}-{val_loss:.5f}.hdf5', monitor='val_loss', verbose=1,mode='min')])
 
 # model.fit(datas,labels)
 #model.save(output_file_name+'.h5')
